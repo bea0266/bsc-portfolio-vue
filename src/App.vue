cc02d017093589edb236a9dc83c19c2c137a16c4
bea0266 <bea0266@naver.com>
2025-01-08 00:43:38 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<h1>Portfolio of FE Developer Seungchan Bae</h1>
+</template>
+
+<style scoped>
+</style>
