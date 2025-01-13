db042fde9b85b8ccc3d7d612c02b45efdcae2c32
bea0266 <bea0266@naver.com>
2025-01-13 23:13:49 +0900

    vue 기본 샘플 화면 삭제, 홈 화면 레이아웃 수정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
 <nav>
   <RouterLink to="/home">Home</RouterLink> |
@@ -14,15 +10,33 @@ import { RouterLink, RouterView } from 'vue-router'
   <span>이전 페이지</span> | 
   <span>다음 페이지</span>
 </div>
-
-
-
-
 </template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 
-<style scoped>
+</script>
+<style>
 #book_area {
   height: 900px;
-  background-color: #c25959;
+  border: 1px solid #333;
 }
+
+nav {
+  display: flex;
+  /* background-color: #f1f1f1; */
+  margin: 5px;
+  padding: 15px;
+  justify-content: space-around;
+  align-items: center;
+  text-align: center;
+}
+
+nav a {
+  text-decoration: none;
+  font-weight: bold;
+  color: #333;
+  
+}
+
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,39 @@
 <template>
     <div class="main_cont">
-        <h1>개발자 배승찬의 이야기</h1>
+        <h1 class="main_cont_title">개발자 배승찬의 이야기</h1>
         <p>안녕하세요 개발자 배승찬입니다. </p>
         <p>바로가기</p>
-        <ul>
-            <li>Introduce</li>
-            <li>Skills</li>
-            <li>Projects</li>
-        </ul>
+        <button @click="linkToPage('introduce')">Introduce</button>
+        <button @click="linkToPage('skills')">Skills</button>
+        <button @click="linkToPage('projects')">Projects</button>
     </div>
 </template>
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const linkToPage = (pageName: string) => {
+  router.push({ name: pageName });
+}
 </script>
+<style>
+.main_cont {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 800px;
+}
+
+button {
+  width: 125px;
+  background-color: #000000;
+  color: white;
+  border-radius: 25px;
+  padding: 5px;
+  margin-bottom: 10px;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
